08a33eccd499976b717f7908a33214b68f0a9883
Piotr Lasek <plasek@sparq.eecs.yorku.ca>
2017-09-13 16:13:01 -0400

    changes to make it possible to run on sparq

--- src/run-kmeans-tests.sh
+++ src/run-kmeans-tests.sh
@@ -1,32 +1,26 @@
-#!/bin/bash
-# declare STRING variable
-STRING="Hello World"
-#print variable on a screen
-echo "Start."
+#!/cs/local/bin/bash
 
-cp='-cp ".:../../../lib/*" Main'
+echo -----------------------------------------------------------------------
+echo Start.
+echo -----------------------------------------------------------------------
 
-wp="workspacepath=\eecs\home\plasek\clustering"
-dt="data=data"
-dtc="data=[CUSTOM]\data\checkins-pyramid"
-al="algorithm"
+m=Main
+cp='.:../../../lib/*'
+
+
+wp="workspacepath=/local/data1/plasek/clustering"
+dt="data=/data"
+dtc="data=[CUSTOM]/data/checkins-pyramid/"
+al=algorithm
 kmeans="$al=kMeans"
 pikmea="$al=pikMeans"
-pr="parameters=dump"
+pr="parameters=\"dump"
 
-f="Checkins"
+f='/Checkins'
 
-echo "java $cp $wp $dtc $pikmea $pr;k:5;maxIterations:10;deepest:15;depth:15;starting:3;plot"
-echo $cp
-java $cp
+#r="java -Xss1024 -Xmx2048 -cp $cp $m $wp $dt$f.txt $kmeans $pr;k:7;maxIterations:10\""
+#$r
 
-java -cp ".:../../../lib/*" Main 
-
-
-# $wp $dtc $pikmea $pr;k:5;maxIterations:10;deepest:15;depth:15;starting:3;plot
-
-
-#java %cp% %wp% %dt%%f%.txt %kmeans% %pr%;k:5;maxIterations:10
-#java %cp% %wp% %dtc%       %pikmea% %pr%;k:5;maxIterations:10;deepest:15;depth:15;starting:3;plot
-
-
+r="java -Xms2048m -Xmx18192m -cp $cp $m $wp $dtc $pikmea $pr;k:5;maxIterations:40;deepest:15;depth:15;starting:3\""
+echo $r
+$r
