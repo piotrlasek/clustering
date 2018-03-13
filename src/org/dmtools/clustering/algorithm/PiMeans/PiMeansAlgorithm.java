5e053ebce520b126dfe4140af864f760e3cfcdcc
Piotr Lasek <piotr.lasek@gmail.com>
2018-03-13 19:50:51 +0100

    adding a condition for better clustering (checking the distance between the "nearest" clusters)

--- src/org/dmtools/clustering/algorithm/PiMeans/PiMeansAlgorithm.java
+++ src/org/dmtools/clustering/algorithm/PiMeans/PiMeansAlgorithm.java
@@ -1,5 +1,6 @@
 package org.dmtools.clustering.algorithm.PiMeans;
 
+import javafx.scene.input.PickResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dmtools.clustering.CDMBasicClusteringAlgorithm;
@@ -46,6 +47,46 @@ public class PiMeansAlgorithm extends CDMBasicClusteringAlgorithm {
         depth = pkmas.getDepth();
     }
 
+    public class BoundManager {
+
+        HashMap<Double, PiCluster> lbDist = new HashMap<>();
+        HashMap<Double, PiCluster> ubDist = new HashMap<>();
+
+        public void putLowerBound(Double lb, PiCluster cluster) {
+            lbDist.put(lb, cluster);
+        }
+
+        public void putUpperBound(Double ub, PiCluster cluster) {
+            ubDist.put(ub, cluster);
+        }
+
+        public PiCluster getNearestClusterByLb() {
+            Double minLbKey = Collections.min(lbDist.keySet());
+            return lbDist.get(minLbKey);
+        }
+
+        public PiCluster getNearestClusterByUb() {
+            Double minUbKey = Collections.min(ubDist.keySet());
+            return ubDist.get(minUbKey);
+        }
+
+        public Double getLbDist() {
+            List<Double> l = new ArrayList(lbDist.keySet());
+            Collections.sort(l);
+            Double lb0 = l.get(0);
+            Double lb1 = l.get(1);
+            return Math.abs(lb0 - lb1);
+        }
+
+        public Double getUbDist() {
+            List<Double> l = new ArrayList(ubDist.keySet());
+            Collections.sort(l);
+            Double ub0 = l.get(0);
+            Double ub1 = l.get(1);
+            return Math.abs(ub0 - ub1);
+        }
+    }
+
 
     /**
      *
@@ -56,25 +97,22 @@ public class PiMeansAlgorithm extends CDMBasicClusteringAlgorithm {
         //  assign bin to a cluster if it hasn't been assigned to any cluster yet
         if (bin.getCluster() == null) {
 
-            HashMap<Double, PiCluster> lbDist = new HashMap<>();
-            HashMap<Double, PiCluster> ubDist = new HashMap<>();
+            BoundManager bm = new BoundManager();
 
             for (PiCluster seed : seeds) {
                 double lb = bin.lowerBound(seed);
                 double ub = bin.upperBound(seed);
-                lbDist.put(lb, seed);
-                ubDist.put(ub, seed);
+                bm.putLowerBound(lb, seed);
+                bm.putUpperBound(ub, seed);
             }
 
-            Double minLbKey = Collections.min(lbDist.keySet());
-            Double minUbKey = Collections.min(ubDist.keySet());
+            PiCluster cLb = bm.getNearestClusterByLb();
+            PiCluster cUb = bm.getNearestClusterByUb();
 
-            PiCluster cLb = lbDist.get(minLbKey);
-            PiCluster cUb = ubDist.get(minUbKey);
 
             //if (minUbKey >= 2*bin.getSize() && minLbKey >= 2* bin.getSize() && cLb == cUb) {
             //if (minUbKey >= 2*bin.getSize() && minLbKey >= 2* bin.getSize() && cLb == cUb) {
-            if (cLb == cUb) {
+            if (cLb == cUb && bm.getLbDist() > 2*bin.getSize()) {
                 // assing the cluster if it is possible
                 log.info("Assign bin " + bin.getZoo() + " on layer " +
                         bin.getLayer() + " to cluster " + cLb.getId());
@@ -105,7 +143,7 @@ public class PiMeansAlgorithm extends CDMBasicClusteringAlgorithm {
         max[0] = Math.pow(2, 16) - 1;
         max[1] = Math.pow(2, 16) - 1;
 
-        int initialLayer = 2;
+        int initialLayer = 1;
 
         timer.indexStart();
 
@@ -123,14 +161,14 @@ public class PiMeansAlgorithm extends CDMBasicClusteringAlgorithm {
         PiCluster s1 = new PiCluster(new double[]{10000, 50000});
         s1.setId(0);
         seeds.add(s1);
-        PiCluster s2 = new PiCluster(new double[]{20000, 45000});
+        PiCluster s2 = new PiCluster(new double[]{20000, 46000});
         s2.setId(1);
         seeds.add(s2);
-        PiCluster s3 = new PiCluster(new double[]{35000, 50000});
+        PiCluster s3 = new PiCluster(new double[]{38500, 50000});
         s3.setId(2);
         seeds.add(s3);
 
-        PiCluster s4 = new PiCluster(new double[]{40000, 45000});
+        PiCluster s4 = new PiCluster(new double[]{52000, 31500});
         s4.setId(3);
         seeds.add(s4);
         // select the initial layer
@@ -144,7 +182,6 @@ public class PiMeansAlgorithm extends CDMBasicClusteringAlgorithm {
 
         layer = picube.getLayer(initialLayer);
         Dump.toFile(Utils.clusteredLayerToString(seeds, layer), "layer" + 4 + "clust.csv");
-
 
         log.info("Number of bins: " + layer.entrySet().size());
 
